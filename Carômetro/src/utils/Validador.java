b38ee5c6b3fe8309a73f1efd612bc6703605d299
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-26 12:47:38 -0300

    Add files via upload
    Andamento

--- Carômetro/src/utils/Validador.java
+++ Carômetro/src/utils/Validador.java
@@ -1,5 +1,28 @@
 package utils;
 
-public class Validador {
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class Validador extends PlainDocument {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int limites;
+
+	public Validador(int limites) {
+		// gerar o contrutor
+		super();
+		this.limites = limites;
+	}
+
+	public void insertString(int ofs, String str, AttributeSet a) throws BadLocationException {
+		if ((getLength() + str.length()) <= limites) {
+			super.insertString(ofs, str, a); // atraves do contrutor("super") vai extenter o recurso nas caixas de texto
+
+		}
+	}
 
 }
--- Carômetro/src/view/Carometro.java
+++ Carômetro/src/view/Carometro.java
@@ -1,30 +1,61 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.SystemColor;
 import java.awt.Toolkit;
-import java.sql.Connection;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import model.DAO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import model.DAO;
+import utils.Validador;
 
 public class Carometro extends JFrame {
-	
-	//instanciar obejtos
-	
+
+	// instanciar objetos JDBC
+
 	DAO dao = new DAO();
-	
+
 	private Connection con;
+
+	// instanciar objeto para o fluxo de bytes
+	private FileInputStream fis;
+
+	// variavel global para armazenar tamanho da imagem(bytes)
+	private int tamanho;
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblStatus;
+	private JLabel lblData;
+	private JLabel lblNewLabel;
+	private JTextField txtRA;
+	private JLabel lblNewLabel_1;
+	private JLabel lblFoto;
 
 	/**
 	 * Launch the application.
@@ -50,6 +81,7 @@ public class Carometro extends JFrame {
 			@Override
 			public void windowActivated(WindowEvent e) {
 				status();
+				setarData();
 			}
 		});
 		setTitle("carometro");
@@ -61,21 +93,113 @@ public class Carometro extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.textHighlight);
+		panel.setBounds(10, 227, 600, 54);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		lblStatus = new JLabel("");
+		lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
+		lblStatus.setBounds(545, 11, 32, 32);
+		panel.add(lblStatus);
+
+		lblData = new JLabel("");
+		lblData.setForeground(SystemColor.text);
+		lblData.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblData.setBounds(10, 11, 503, 32);
+		panel.add(lblData);
+
+		lblNewLabel = new JLabel("RA");
+		lblNewLabel.setBounds(27, 32, 61, 14);
+		contentPane.add(lblNewLabel);
+
+		txtRA = new JTextField();
+		txtRA.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				String caracteres = "0123456789";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
+		txtRA.setBounds(76, 29, 96, 20);
+		contentPane.add(txtRA);
+		txtRA.setColumns(10);
+		// uso do PlainDocumente para limitar os campos
+		txtRA.setDocument(new Validador(6));
+
+		lblNewLabel_1 = new JLabel("Nome");
+		lblNewLabel_1.setBounds(27, 69, 49, 14);
+		contentPane.add(lblNewLabel_1);
+
+		JTextField txtNome = new JTextField();
+		txtNome.setBounds(76, 63, 220, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		// uso do PlainDocumente para limitar os campos
+		txtNome.setDocument(new Validador(30));
+
+		lblFoto = new JLabel("");
+		lblFoto.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		lblFoto.setIcon(new ImageIcon(Carometro.class.getResource("/img/camera.png")));
+		lblFoto.setBounds(354, 0, 256, 216);
+		contentPane.add(lblFoto);
+
+		JButton btnCarregar = new JButton("Carregar Foto");
+		btnCarregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				carregarFoto();
+			}
+		});
+		btnCarregar.setForeground(SystemColor.textHighlight);
+		btnCarregar.setBounds(211, 94, 133, 23);
+		contentPane.add(btnCarregar);
+
 	}// fim do construtor
 
 	private void status() {
 		try {
 			con = dao.conectar();
-			if(con == null) {
-				System.out.print("Erro de Conexão");
-			}else {
-				System.out.print("Banco conectado");
+			if (con == null) {
+				// System.out.print("Erro de Conexão");
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
+			} else {
+				// System.out.print("Banco conectado");
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dbon.png")));
 			}
 			con.close();
 		} catch (Exception e) {
 			System.out.print(e);
 		}
 	}
-	
-	
+
+	private void setarData() {
+		// instanciar
+		Date data = new Date();
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		lblData.setText(formatador.format(data));
+	}
+
+	private void carregarFoto() {
+		JFileChooser jfc = new JFileChooser();
+		jfc.setDialogTitle("Selecionar arquivo");
+		jfc.setFileFilter(new FileNameExtensionFilter("Arquivo de Imagens(*.PNG,*.JPG,*.JPEG)", "png", "jpg", "jpeg"));
+		int resultado = jfc.showOpenDialog(this);
+		if (resultado == JFileChooser.APPROVE_OPTION) {
+			try {
+				fis = new FileInputStream(jfc.getSelectedFile());
+				tamanho = (int) jfc.getSelectedFile().length();
+				Image foto = ImageIO.read(jfc.getSelectedFile()).getScaledInstance(lblFoto.getWidth(),
+						lblFoto.getHeight(), Image.SCALE_SMOOTH);
+				lblFoto.setIcon(new ImageIcon(foto));
+				lblFoto.updateUI();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
 }
